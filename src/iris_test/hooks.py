73f2d7b78dcb21cfdf885654d26f959fbc7ae821
Tomoyuki Nohara <>
2021-06-19 23:22:38 +0900

    :D

---
+++ src/iris_test/hooks.py
@@ -0,0 +1,80 @@
+"""Project hooks."""
+from typing import Any, Dict, Iterable, Optional
+
+from kedro.config import ConfigLoader
+from kedro.framework.hooks import hook_impl
+from kedro.io import DataCatalog
+from kedro.versioning import Journal
+from typing import Any, Dict
+
+import mlflow
+# import mlflow.sklearn
+from kedro.framework.hooks import hook_impl
+from kedro.pipeline.node import Node
+
+
+class ProjectHooks:
+    @hook_impl
+    def register_config_loader(
+        self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any]
+    ) -> ConfigLoader:
+        return ConfigLoader(conf_paths)
+
+    @hook_impl
+    def register_catalog(
+        self,
+        catalog: Optional[Dict[str, Dict[str, Any]]],
+        credentials: Dict[str, Dict[str, Any]],
+        load_versions: Dict[str, str],
+        save_version: str,
+        journal: Journal,
+    ) -> DataCatalog:
+        return DataCatalog.from_config(
+            catalog, credentials, load_versions, save_version, journal
+        )
+
+
+# class ModelTrackingHooks:
+    """Namespace for grouping all model-tracking hooks with MLflow together."""
+
+    @hook_impl
+    def before_pipeline_run(self, run_params: Dict[str, Any]) -> None:
+        """Hook implementation to start an MLflow run
+        with the same run_id as the Kedro pipeline run.
+        """
+        mlflow.start_run(run_name=run_params["run_id"])
+        mlflow.log_params(run_params)
+
+    @hook_impl
+    def after_node_run(
+        self, node: Node, outputs: Dict[str, Any], inputs: Dict[str, Any]
+    ) -> None:
+        """Hook implementation to add model tracking after some node runs.
+        In this example, we will:
+        * Log the parameters after the data splitting node runs.
+        * Log the model after the model training node runs.
+        * Log the model's metrics after the model evaluating node runs.
+        """
+        if node._func_name == "split_data":
+            mlflow.log_params(
+                {"split_data_ratio": inputs["params:example_test_data_ratio"]}
+            )
+
+        elif node._func_name == "train_model":
+            model = outputs["example_model"]
+            mlflow.sklearn.log_model(model, "model")
+            # mlflow.log_model(model, "model")
+            mlflow.log_params(inputs["parameters"])
+
+    @hook_impl
+    def after_pipeline_run(self) -> None:
+        """Hook implementation to end the MLflow run
+        after the Kedro pipeline finishes.
+        """
+        mlflow.end_run()
+
+
+
+
+
+
